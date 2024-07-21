c69a1b46bbd3a86071bbd2f7803f970b1c1b798c
DianTrihastama <164548755+DianTrihastama@users.noreply.github.com>
2024-07-21 15:52:21 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const audio =
+document  = document () ;
+
+audio.addEventListener('play') , () => {
+    body.classlist.add ('play music') ;
+};
+
+audio.addEventListener('pause') () => {
+    body.classlist.remove('play music');
+}
+
